1c4e3fb8deb9f17b2abfb14f614a39fa07a744ca
DESKTOP-GFKIBM8\Jose Carlos Alvarez <jose1019_alva@outlook.com>
2021-09-16 02:08:42 -0500

    First Commit

---
+++ HttpClientDemo/Models/WeatherForecastModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpClientDemo.Models
+{
+    public class WeatherForecastModel
+    {
+        public DayForecastModel[] consolidated_weather { get; set; }
+        public DateTime Sun_rise { get; set; } // sunRise -> SunRise (json to C#)
+        public DateTime Sun_set { get; set; }
+        public string Title { get; set; }
+        public string Timezone { get; set; }
+    }
+
+
+}
